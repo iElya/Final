5d4952e04c70d00559f2b2f3a2d1fe95a45254a3
iElya <mdv.elya@gmail.com>
2022-12-21 23:01:08 +0400

    FINAL

--- js/index.js
+++ js/index.js
@@ -1,6 +1,1 @@
 // navbar scroll
-var e_nav1 = document.querySelector(".e_nav1");
-window.onscroll = () => {
-    this.scrollY > 20 ? e_nav1.classList.add("sticky"):
-    e_nav1.classList.remove("sticky")
-}
