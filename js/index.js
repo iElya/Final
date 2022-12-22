ddf1f7ae2030f901efcb1aae4a200551028cb19f
iElya <mdv.elya@gmail.com>
2022-12-22 10:36:25 +0400

    main

--- js/index.js
+++ js/index.js
@@ -1,1 +1,24 @@
 // navbar scroll
+
+window.onscroll = () => {
+    let nav = document.querySelector('nav');
+    if(window.pageYOffset > 0){
+        nav.classList.add('sticky');
+    }
+    else{
+        nav.classList.remove('sticky');
+
+    }
+}
+
+
+
+// typing text
+var typed = new Typed(".auto-type" , {
+    strings: ["I'm Jonathon Doe" , "I'm Web Developer" , "Lives in UK"],
+    typeSpeed: 200,
+    backSpeed: 50,
+    startDelay: 500,
+    loop: true,
+
+})
