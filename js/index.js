8785c2309c44224edac90993d7a9df90ca9f4ea1
iElya <mdv.elya@gmail.com>
2022-12-22 14:51:50 +0400

    FINAL

--- js/index.js
+++ js/index.js
@@ -22,3 +22,21 @@ var typed = new Typed(".auto-type" , {
     loop: true,
 
 })
+
+// carusel 
+var swiper = new Swiper(".mySwiper", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
